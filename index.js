7c54640fe7ba04414ad4a5edc3db7ac66da7063b
Jessica Wang <xuecen.wang@gmail.com>
2023-09-19 17:58:18 -0400

    completed static website and major features

--- index.js
+++ index.js
@@ -224,8 +224,6 @@ const tesBtnRight = document.querySelector("#tes-right");
 const testimony = document.querySelector(".testimony-text");
 const numberTab = document.querySelectorAll(".tab-item");
 
-console.log(numberTab);
-
 tesBtnLeft.addEventListener("click", () => {
   let current;
   for (let i = 0; i < 5; i++) {
@@ -263,4 +261,202 @@ tesBtnRight.addEventListener("click", () => {
   numberTab[next].classList.add("active");
   testimony.children[0].innerHTML = testimonyData[next].message;
   testimony.children[1].innerHTML = testimonyData[next].name;
+});
+
+
+const popularCountryData = new Map();
+const egyptData = [
+  {
+    img: "url(assets/images/greece-size-small.jpg)",
+    city: "Bahariya Oasis",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sollicitudin eget.",
+    rate: 6400,
+  },
+  {
+    img: "url(assets/images/greece-size-small.jpg)",
+    city: "Sahl Hasheesh",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sollicitudin eget.",
+    rate: 7700,
+  },
+  {
+    img: "url(assets/images/greece-size-small.jpg)",
+    city: "Alexandria",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sollicitudin eget.",
+    rate: 5300,
+  },
+];
+
+const greeceData = [
+  {
+    img: "url(./assets/images/greece-size-small.jpg)",
+    city: "Palaiokastritsa",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sollicitudin eget.",
+    rate: 5600,
+  },
+  {
+    img: "url(./assets/images/greece-size-small.jpg)",
+    city: "Meteora",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sollicitudin eget.",
+    rate: 2800,
+  },
+  {
+    img: "url(./assets/images/greece-size-small.jpg)",
+    city: "Thessaloniki",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sollicitudin eget.",
+    rate: 6700,
+  },
+];
+
+const indonesiaData = [
+  {
+    img: "url(./assets/images/greece-size-small.jpg)",
+    city: "Canggu",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sollicitudin eget.",
+    rate: 6500,
+  },
+  {
+    img: "url(./assets/images/greece-size-small.jpg)",
+    city: "Komodo",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sollicitudin eget.",
+    rate: 7600,
+  },
+  {
+    img: "url(./assets/images/greece-size-small.jpg)",
+    city: "Penida Island",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sollicitudin eget.",
+    rate: 2800,
+  },
+];
+
+const franceData = [
+  {
+    img: "url(./assets/images/greece-size-small.jpg)",
+    city: "Bonifacio",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sollicitudin eget.",
+    rate: 1400,
+  },
+  {
+    img: "url(./assets/images/greece-size-small.jpg)",
+    city: "Brittany",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sollicitudin eget.",
+    rate: 3600,
+  },
+  {
+    img: "url(./assets/images/greece-size-small.jpg)",
+    city: "Carcassonne",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sollicitudin eget.",
+    rate: 3900,
+  },
+];
+
+popularCountryData.set(1, egyptData);
+popularCountryData.set(2, greeceData);
+popularCountryData.set(3, indonesiaData);
+popularCountryData.set(4, franceData);
+
+console.log(popularCountryData);
+
+const activeBar = document.querySelector(".the-bar");
+const tabDots = document.querySelector(".tab-dots");
+const innerDots = document.querySelector(".inner-dots");
+const innerDotsList = document.querySelectorAll(".dot");
+const pageList = document.querySelectorAll(".page");
+
+console.log(pageList)
+
+tabDots.addEventListener("click", (e) => {
+  if (e.target.tagName === "LI") {
+    const current = Number(e.target.dataset.index);
+    const tabDotsList = tabDots.children;
+
+    for (let i = 0; i < tabDotsList.length; i++) {
+        tabDotsList[i].classList.remove("active");
+        pageList[i].classList.remove("active");
+    }
+
+    e.target.classList.add("active");
+    pageList[current - 1].classList.add("active");
+
+    for (let i = 0; i < innerDotsList.length; i++) {
+      if (current && innerDotsList[i].dataset.index > current) {
+        innerDotsList[i].classList.remove("active");
+      } else {
+        innerDotsList[i].classList.add("active");
+      }
+    }
+
+    activeBar.style.backgroundImage = changeBarBkgColor(current);
+
+    // const currentData = popularCountryData.get(current);
+    // console.log(currentData);
+    // for (let i = 0; i < cityInfoList.length; i++) {
+    //   const infoList = cityInfoList[i].children;
+    //   console.log(`info`,infoList);
+    //   infoList[0].style.backgroundImage = currentData[i].img;
+    //   infoList[1].innerHTML = currentData[i].city;
+    //   infoList[2].innerHTML = currentData[i].description;
+    //   infoList[4].innerHTML = `Rates from ${currentData[i].rate}`;
+    // }
+
+  }
+});
+
+function changeBarBkgColor(eventIndex) {
+  return `linear-gradient(to right, black 0 ${
+    25 * eventIndex
+  }%, red ${eventIndex}% 100%)`;
+}
+
+
+const outers = document.querySelectorAll(".outer");
+const inners = document.querySelectorAll(".inner");
+const boxs = document.querySelectorAll(".box");
+window.addEventListener("scroll", function (e) {
+  const scrollY = window.scrollY;
+  console.log(scrollY);
+  if (scrollY > 200) {
+    boxs[0].classList.add("active");
+    inners[0].classList.add("active");
+    outers[0].classList.add("active");
+  }
+  if (scrollY > 400) {
+    boxs[1].classList.add("active");
+    inners[1].classList.add("active");
+    outers[1].classList.add("active");
+  }
+  if (scrollY > 800) {
+    boxs[2].classList.add("active");
+    inners[2].classList.add("active");
+    outers[2].classList.add("active");
+  }
+  if (scrollY > 1200) {
+    boxs[3].classList.add("active");
+    inners[3].classList.add("active");
+    outers[3].classList.add("active");
+  }
+});
+
+
+const aboutBox = document.querySelector(".gallery");
+const lastBox = document.querySelector(".gallery .item:last-child");
+// console.log(lastBox.offsetLeft);
+// console.log(window.innerWidth);
+aboutBox.addEventListener("mousemove", function (e) {
+    if (e.clientX <= window.innerWidth / 2) {
+            aboutBox.style.marginLeft = "-80px";
+    } else {
+            aboutBox.style.marginLeft = "-900px";
+      }
 });
